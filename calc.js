cec975602dc76df1ab0a13e0fa313d53244a82cd
Kyatramoni Rakesh <93992093+KyatramoniRakesh@users.noreply.github.com>
2024-01-22 16:14:53 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,36 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    let result = document.getElementById("result");
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].onclick = function () {
+            let input = this.innerText;
+            updateDisplay(input);
+        };
+    }
+
+    function updateDisplay(input) {
+        let currentDisplay = result.innerText;
+
+        if (currentDisplay === "0") {
+            if (input !== "C" && input !== "DEL" && input !== "=") {
+                result.innerText = input;
+            }
+        } else {
+            if (input === "DEL") {
+                result.innerText = currentDisplay.substring(0, currentDisplay.length - 1);
+            } else if (input === "C") {
+                result.innerText = "0";
+            } else if (input !== "C" && input !== "DEL" && input !== "=") {
+                result.innerText += input;
+            }
+        }
+
+        if (input === "=") {
+            let display = result.innerText;
+            result.innerText = eval(display);
+        }
+    }
+});
